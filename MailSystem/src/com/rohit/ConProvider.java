d4cf5995ddd05f4413b6f1e51d7a0ecb4e9d2a49
DELL <DELL@DAKSHESH>
2020-06-16 10:28:43 +0530

    First Commit
    Uploading all files

---
+++ MailSystem/src/com/rohit/ConProvider.java
@@ -0,0 +1,15 @@
+package com.rohit;
+
+import java.sql.*;
+public class ConProvider {
+
+	public static Connection getConnection(){
+	Connection con=null;
+	try{
+		Class.forName("com.mysql.jdbc.Driver");
+		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "root", "");
+	}catch(Exception e){System.out.println(e);}
+	return con;
+    }
+}
+
